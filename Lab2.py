b1164c6d1b83c2ad1a6ba5239857dd3ae4d2360d
Alvert <bloxoman88@gmail.com>
2025-06-17 16:58:35 -0700

    lab2 work

---
+++ Lab2.py
@@ -0,0 +1,39 @@
+# Lab 2: Python Profiles
+
+## step 1 name variable
+
+name = "Alvert Lin"
+
+## Step 2 nickname variable
+nickname = 'Al'
+
+## Step 3 age variable
+age = 16
+
+## Step 4 boolean variable
+used_python = False 
+
+## Step 5 List variable 
+hobbies = ["Gaming", "Learning", "Exercising", "Listening to Music"]
+
+## Step 6 Dictionary variable 
+favorite_things = {"Running": "Track and Cross Country", "Games": "Roblox", "Food": ["Sushi", "Udon", "Pizza"], "Sports": ["Track", "Cross Country", "Volleyball"]}
+
+## {key:value}
+
+## Step 7: Set variable 
+teachers = {"parker b", "parker b", "mia"}
+
+## step 7.1 Tuple  variables 
+teachers = ("parker b", "parker b", "mia")
+
+print(name + nickname)
+print(hobbies)
+print(favorite_things)
+print(teachers) 
+
+
+
+
+
+
