5f0a97171346e255748d029ec6309edff195b1ff
Alvert <bloxoman88@gmail.com>
2025-06-30 13:21:25 -0700

    Im back

---
+++ Lab4.py
@@ -0,0 +1,38 @@
+#Checking = "yes"
+
+#while Checking == "yes": 
+ #   print("what is your age: ")
+ #   user_input = input()
+ #   if int(user_input) >= 18: 
+  #      print("yes you can vote")
+#else:
+#    print("you can't vote")
+#print("would you like to check another age?")
+#user_input2 = input()
+#checking = user_input2
+
+#list1 = [3, -1, 0, 6, -4]
+
+#for x in list1:
+ #   if x > 0:
+ #       print("value is positive")
+#   elif x < 0: 
+ #       print("value is negative")
+ #   else:
+ #        print("number is zero")
+
+
+inventory = ["tnt", "glass", "grass", "bedrock", "End Crystal", "netherite"]
+
+#for i in inventory:
+ #   if i == "bedrock":
+ #       print("why do you have this in your inventory?")
+ #   elif i == "tnt":
+ #       print("Go bomb a House")
+
+
+for i in inventory: 
+    if i == "netherite":
+        print("Metal")
+    elif i == "tnt":
+        print("boom")
