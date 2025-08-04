31289221109150e456ec40702055b1d64a88bed4
Alvert <bloxoman88@gmail.com>
2025-08-04 14:30:43 -0700

    reupload

--- Lab4.py
+++ Lab4.py
@@ -1,39 +1,35 @@
-#Checking = "yes"
+Checking = "yes"
 
-#while Checking == "yes": 
-  #  print("what is your age")
-  #  user_input = input()
+while Checking == "yes": 
+  print("what is your age")
+  user_input = input()
+    
+  if int(user_input) >= 18:
+      print("yes you can vote")
+      break
+  elif int(user_input) < 18:
+     print("You are not old enough")
+     break
+  
 
-  #  if int(user_input) >= 18: 
-   #     print("yes you can vote")
-  #  elif int(user_input) < 18:
-  #      print("You are not old enough")
+list1 = [3, -1, 0, 6, -4]
 
-
-#else:
- #       print("you can't vote")
-#print("would you like to check another age?")
-#user_input2 = input()
-#checking = user_input2
-
-#list1 = [3, -1, 0, 6, -4]
-
-#for x in list1:
- #   if x > 0:
- #       print("value is positive")
-#   elif x < 0: 
- #       print("value is negative")
- #   else:
- #        print("number is zero")
+for x in list1:
+    if x > 0:
+        print("value is positive")
+    elif x < 0: 
+        print("value is negative")
+    else:
+         print("number is zero")
 
 
 inventory = ["tnt", "glass", "grass", "bedrock", "End Crystal", "netherite"]
 
-#for i in inventory:
- #   if i == "bedrock":
- #       print("why do you have this in your inventory?")
- #   elif i == "tnt":
- #       print("Go bomb a House")
+for i in inventory:
+    if i == "bedrock":
+        print("why do you have this in your inventory?")
+    elif i == "tnt":
+        print("Go bomb a House")
 
 
 for i in inventory: 
